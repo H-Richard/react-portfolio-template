4ace6ca6f8a5322dc9a127e83526259f42b073fc
H-Richard <r.hong@protonmail.com>
2019-01-26 18:49:26 -0500

    initial

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navigation from "./Modules/Navigation";
+//import "insert object" from "./Modules/insert object";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+      </div>
+    );
+  }
+};
+
+export default App;
